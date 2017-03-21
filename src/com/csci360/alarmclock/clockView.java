e4951da3a8fd69082ba41f0f721c0f86951a3a0a
mikisugimoto <burlesonsugimotome@g.cofc.edu>
2017-03-21 13:01:41 -0400

    Add files via upload

---
+++ src/com/csci360/alarmclock/clockView.java
@@ -0,0 +1,15 @@
+package com.csci360.alarmclock;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Miki
+ */
+public class clockView {
+    
+    public void viewClock(theClock view) {
+        System.out.println("Current time is " + view.toString());
+    }
+    
+}
