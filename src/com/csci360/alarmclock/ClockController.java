55337bbfbd59865edfe9177bd909485a18887304
mikisugimoto <burlesonsugimotome@g.cofc.edu>
2017-03-28 09:58:37 -0400

    Add files via upload

--- src/com/csci360/alarmclock/ClockController.java
+++ src/com/csci360/alarmclock/ClockController.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.csci360.alarmclock;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  *
- * @author benjaminmuldrow
+ * @author miki
  */
 public class ClockController {
+        
+    Clock clock;
+    
+    public ClockController() {
+        clock = new Clock();
+    }
+    
+    public void incrementMinutes() {
+        clock.setMinutes(clock.getMinutes() + 1);
+    }
+    
+    public void incrementHours() {
+        clock.setHours(clock.getHours() + 1);
+    }
+    
+    public void decrementMinutes() {
+        clock.setMinutes(clock.getMinutes() - 1);
+    }
+    
+    public void decrementHours() {
+        clock.setHours(clock.getHours() - 1);
+    }
+    
+    public String getCurrentTime() {
+        return clock.getTime();
+    }
     
 }
---
+++ src/com/csci360/alarmclock/ClockView.java
@@ -0,0 +1,15 @@
+package com.csci360.alarmclock;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Miki
+ */
+public class ClockView {
+    
+    public void viewClock(Clock view) {
+        System.out.println("Current time is " + view.toString());
+    }
+    
+}
